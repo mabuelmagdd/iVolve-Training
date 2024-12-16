a2754d1fc97303d81fb887e6a7e5b53824a93a8d
mabuelmagdd <93723941+mabuelmagdd@users.noreply.github.com>
2024-12-16 20:25:15 +0000

    Update deployK8s.groovy

--- Jenkins/Lab 24 - Jenkins Shared Libraries/src/vars/deployK8s.groovy
+++ Jenkins/Lab 24 - Jenkins Shared Libraries/src/vars/deployK8s.groovy
@@ -1,6 +1,11 @@
 def call(String deploymentFile) {
     sh '''
     export KUBECONFIG=~/.kube/config
+    # Check if the file exists
+            ls -l ${DEPLOYMENT_FILE}
+            
+            # Apply the deployment file
+            echo "Applying Kubernetes deployment file: ${DEPLOYMENT_FILE}"
     kubectl apply -f ${deploymentFile}
     '''
 }
